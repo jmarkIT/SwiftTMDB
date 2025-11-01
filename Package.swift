f9a7ffc4c3c02004aed2f29989672996c228da45
James Mark <james.david.mark@gmail.com>
2025-11-01 16:42:09 -0500

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftTMDB",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SwiftTMDB",
+            targets: ["SwiftTMDB"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SwiftTMDB"
+        ),
+        .testTarget(
+            name: "SwiftTMDBTests",
+            dependencies: ["SwiftTMDB"]
+        ),
+    ]
+)
