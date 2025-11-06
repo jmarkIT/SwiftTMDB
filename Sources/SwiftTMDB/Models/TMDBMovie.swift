3078a7d9bd53d457d34842a4ffe8b59f7be3a41e
James Mark <james.david.mark@gmail.com>
2025-11-05 21:27:55 -0600

    Undoing last change, it introduces complexity for no benefit at this time.

--- Sources/SwiftTMDB/Models/TMDBMovie.swift
+++ Sources/SwiftTMDB/Models/TMDBMovie.swift
@@ -15,32 +15,9 @@ public struct TMDBMovie: Codable {
     public let runtime: Int
 }
 
-public enum Genre: Int, Codable {
-    case action = 28
-    case adventure = 12
-    case animation = 16
-    case comedy = 35
-    case crime = 80
-    case documentary = 99
-    case drama = 18
-    case family = 10751
-    case fantasy = 14
-    case history = 36
-    case horror = 27
-    case music = 10402
-    case mystery = 9648
-    case romance = 10749
-    case scienceFiction = 878
-    case tvMovie = 10770
-    case thriller = 53
-    case war = 10752
-    case western = 37
-    case unknown
-    
-    public init(from decoder: Decoder) throws {
-        let value = try decoder.singleValueContainer().decode(Int.self)
-        self = Genre(rawValue: value) ?? .unknown
-    }
+public struct Genre: Codable {
+    public let id: Int
+    public let name: String
 }
 
 public struct Credits: Codable {
